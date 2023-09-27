1d031975f8460023b2ceb8861ecc0885d4f9cc06
t-softwareengineer <t.softwareengineer@hotmail.com>
2023-09-27 17:33:01 +0100

    build: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+const metaContent: MetaContent = {
+  text: '0P3NS0URCE PR0J3CT5',
+  title: 'PORTFOLIO.IO',
+  version: 'Version 1.0.0-alpha',
+}
+
 export default function HomePage() {
-  return (<h1 className="display-1">Hello World!</h1>);
+  return (
+    <div className="container-100vh">
+      <div className="center-xy">
+        <small className="fw-bold">{metaContent.text}</small>
+        <h1 className="pb-3">{metaContent.title}</h1>
+        <a 
+          className="btn btn-outline-light text-capitalize fw-bold" 
+          href="#"
+        > Coming Soon
+        </a>
+        <div className="d-flex justify-content-end mt-5">
+          <small className="fw-bold">&#x1F4E6; {metaContent.version}</small>
+        </div>
+      </div>
+    </div>
+  );
 }
